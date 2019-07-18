ef0327fb84ec1c533e5ee9811c13ebe86079cf7c
smalghate7867 <smalghate7867@gmail.com>
2019-07-18 19:26:25 +0530

    rest API with angular crud

---
+++ talent-hunt/src/app/view-schedule/view-schedule.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ViewSchedule } from '../view-schedule';
+import { AddScheduleServiceService } from '../add-schedule-service.service';
+
+@Component({
+  selector: 'app-view-schedule',
+  templateUrl: './view-schedule.component.html',
+  styleUrls: ['./view-schedule.component.css']
+})
+export class ViewScheduleComponent implements OnInit {
+scData:Observable<ViewSchedule[]>
+  constructor(private httpViewService:AddScheduleServiceService) { }
+
+  selectData={
+      id:0,
+      cname:'',
+      email:'',
+      cctc:'',
+      ectc:'',
+      gender:'',
+      currentOrg:'',
+      date:null,
+      totalExp:'',
+      releventExp:'',
+      noticePeriod:'',
+      stack:'',
+      location:'',
+      contactNo:'',
+      relocate:'',
+      interviewPannel:'',
+  };
+  reload(){
+    this.httpViewService.findAll().subscribe(data=>{
+      console.log(data)
+      this.scData=data;
+    })
+  }
+
+  deleteUser(id){
+    this.httpViewService.deleteUser(id).subscribe(data=>{
+      this.reload();
+    })
+  }
+
+  getalldata(user){
+   this.selectData=user;
+  //  console.log("in get all data to view schedule "+this.selectData)
+  }
+updateData(form){
+  console.log(form)
+  this.httpViewService.updateUser(form.value).subscribe(data=>{
+    
+  })
+}
+
+  ngOnInit() {
+    this.reload();
+  }
+
+}
